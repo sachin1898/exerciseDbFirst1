a30cff0f9161f1e6bb47c484080e2efa6eddd7e7
sachin <sachinkumars1998@gmail.com>
2021-03-16 15:20:28 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppDBFirst2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
