e351b4e1b82e66f1f349e19830b9e6764e141e59
sachin <sachinkumars1998@gmail.com>
2021-03-16 15:46:24 +0530

    final commit

---
+++ Models/StudentsDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebAppDBFirst2.Models
+{
+    public partial class StudentsDbContext : DbContext
+    {
+        public StudentsDbContext()
+        {
+        }
+
+        public StudentsDbContext(DbContextOptions<StudentsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=DESKTOP-H1QGP17;database=StudentsDb;trusted_connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Contact).HasMaxLength(10);
+
+                entity.Property(e => e.Course).HasMaxLength(50);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
